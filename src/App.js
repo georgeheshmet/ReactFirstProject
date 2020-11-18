5f7ca1a2b08e188e098ef86149e7fe708e2c240e
george heshmet <georgeheshmet@gmail.com>
2020-11-19 00:17:37 +0200

    adde check for no imagelinks

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class Book extends React.Component {
       
       <div className="book">
       <div className="book-top">
-        {book.imageLinks.smallThumbnail!==undefined&&
+        {book.imageLinks!==undefined&&
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>}
         <div className="book-shelf-changer">
         <select value={this.state.BookCat} onChange={(event)=>this.HandelUpdate(book,event.target.value)}>
